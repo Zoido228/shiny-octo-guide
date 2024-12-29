af7d2896d5ff431c2b3c672c935765d9539708ef
Zoido228 <zoido228@gmail.com>
2024-12-30 02:01:03 +0300

    Update kfm.lua

--- kfm.lua
+++ kfm.lua
@@ -1,4 +1,1 @@
-local suskey = "zlhUni6ljtzrgnUQcFpBIoOGOvYli34ASoyYx4ledmN6l3bVjA5UmUBzwoCH"
-function lk(key)
-return key
-end
+return "zlhUni6ljtzrgnUQcFpBIoOGOvYli34ASoyYx4ledmN6l3bVjA5UmUBzwoCH"
